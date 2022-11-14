dc9229e34f8df856d2e690c5c85579d96c393189
anwar <smmaniac@hotmail.co.uk>
2022-11-14 16:33:41 +0000

    const added

--- control/getCategories.js
+++ control/getCategories.js
@@ -1,6 +1,6 @@
 const database = require('../models/getdb.js')
 
-gamesCategories = function(req, res) {
+const gamesCategories = function(req, res) {
     database.getCategories().then(function(currentData) {
         res.status(200).send({message: 'ALL OK', currentData},)
     })
