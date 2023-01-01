0d19cee33e52da2c5c1db531ef912327795ebe27
anwar <smmaniac@hotmail.co.uk>
2023-01-01 17:35:41 +0000

    changed listen

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const app = require('./app');
+// const app = require('./app');
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`It's running on ${port}`);
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`It's running on ${port}`);
+// });
+
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, function () {
+    return console.log(`Listening on ${PORT}...`);
 });
